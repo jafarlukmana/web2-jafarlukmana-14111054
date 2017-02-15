c09eb74f277d7a9aa62503f690ad83efe3faddc7
jafarlukmana <jafarlukmana@gmail.com>
2017-02-15 16:36:57 +0700

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,35 @@
                 <div class="panel-heading">Dashboard</div>
 
                 <div class="panel-body">
-                    You are logged in!
+
+	<header>
+		<h1>CRUD Operation</h1>
+	</header>
+	<nav>
+		<ul>
+			<li><b>MENU</b></li>
+			<li><a href="#">Menu 1</a></li>
+			<li><a href="#">Menu 2</a></li>
+			<li><a href="#">Menu 3</a></li>
+			<li><a href="#">Menu 4</a></li>
+		</ul>
+	</nav>
+	<article>
+		<table border="1" width="75%" align="center">
+			<tr>
+				<th><a href="tambah.php"><button type="submit">+ TAMBAH</button></a></th>
+			</tr>
+			<tr>
+				<td>No</td>
+				<td>Nama</td>
+				<td>Nim</td>
+				<td>Alamat</td>
+				<td>Aksi</td>
+			</tr>
+		</table>
+	</article>
+
+
                 </div>
             </div>
         </div>
